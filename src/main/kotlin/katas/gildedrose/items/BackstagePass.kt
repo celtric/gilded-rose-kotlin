b72826172c1006687cd962baa3a07eaebe3170ec
Ricard Osorio <celtric@gmail.com>
2017-09-30 10:58:29 +0200

    Unify BackstagePass quality management in a single "when" block

--- src/main/kotlin/katas/gildedrose/items/BackstagePass.kt
+++ src/main/kotlin/katas/gildedrose/items/BackstagePass.kt
@@ -1,18 +1,26 @@
 package katas.gildedrose.items
 
-import katas.gildedrose.*
+import katas.gildedrose.Item
+import katas.gildedrose.increaseQuality
+import katas.gildedrose.lostValue
 
 class BackstagePass(item: Item) : PerishableItem(item) {
 
     override fun timeHasPassed() {
-        item.increaseQuality(by = when {
-            item.expiresWithin(0..4) -> 3
-            item.expiresWithin(5..9) -> 2
-            else -> 1
-        })
-
-        if (item.isPastSellDate()) {
-            item.lostValue()
+        when (item.sellIn) {
+            // TODO: how can I express "less than 0" in this case?
+            in -99..-1 -> item.lostValue()
+               in 0..4 -> item.increaseQuality(3)
+               in 5..9 -> item.increaseQuality(2)
+                  else -> item.increaseQuality()
         }
+
+        // Alternative approach:
+        // when  {
+        //     item.isPastSellDate()    -> item.lostValue()
+        //     item.expiresWithin(0..4) -> item.increaseQuality(3)
+        //     item.expiresWithin(5..9) -> item.increaseQuality(2)
+        //                         else -> item.increaseQuality()
+        // }
     }
 }
